09542f5f6434b74d0e39ac2aa52100344c254162
focaloid-dev <74759660+focaloid-dev@users.noreply.github.com>
2023-02-14 17:17:26 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-f2znsf
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-f2znsf)
---
+++ app.config.ts
@@ -0,0 +1,34 @@
+export default defineAppConfig({
+  docus: {
+    title: 'Docus',
+    description: 'The best place to start your documentation.',
+    image:
+      'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+    socials: {
+      twitter: 'nuxt_themes',
+      github: 'nuxt-themes/docus',
+    },
+    aside: {
+      level: 0,
+      exclude: [],
+    },
+    header: {
+      logo: true,
+      showLinkIcon: true,
+      exclude: [],
+    },
+    github: {
+      root: 'content',
+      edit: true,
+      contributors: false,
+    },
+    footer: {
+      iconLinks: [
+        {
+          href: 'https://nuxt.com',
+          icon: 'IconNuxtLabs',
+        },
+      ],
+    },
+  },
+});
